ba42ff6dd824d38a4a4a6eea00e156ec2efb9151
sushil2817 <tony281723@gmail.com>
2024-05-28 10:17:50 +0530

    trying to create and understanding  a big project

---
+++ src/appWrite/auth.js
@@ -0,0 +1,36 @@
+import conf from './conf.js';
+import { Client, Account, ID } from "appwrite";
+
+
+
+export class AuthService{
+    client  = new Client();
+    account ;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email,password,name}){
+        try {
+          const userAccount =  await this.account.create(ID.unique(),email,password,name);
+            
+            if(userAccount){
+                // return userAccount
+                // call another method
+            }else{
+                return 
+            }
+
+        } catch (error) {
+              throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
