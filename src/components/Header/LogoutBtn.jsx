7b91809bbc6964eefdaf2b1b96021a15e34f3027
sushil2817 <tony281723@gmail.com>
2024-06-03 08:30:52 +0530

    making signin and sign up components

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appWrite/config'
+import {logout} from '../../store/authSlice'
+
+
+function LogoutBtn() {
+    const dispatch = useDispatch()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LogoutBtn
