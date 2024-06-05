bbe956cc4f93e3653371af07eb7651c92e6f1b58
sushil2817 <tony281723@gmail.com>
2024-06-05 08:48:21 +0530

    one step forward to project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -34,6 +34,7 @@ function Header() {
       active:authStatus
     }
   ]
+
   return (
     <div>
       <header className='py-3 shrink bg-gray-500'>
@@ -44,12 +45,29 @@ function Header() {
                 <Logo width='70px'></Logo>
               </link>
             </div>
-            
+              <ul className='flex
+              ml-auto'>
+                {navItems.map((item)=>
+                    item.active?(
+                      <li key={item.name}>
+                        <button onClick={()=>navigate(item.slug)}
+                        className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                        >{item.name}</button>
+                      </li>
+                    ):null
+                )}
+                { authStatus && (
+                  <li>
+                    <LogoutBtn />
+                  </li>
+                ) }
+              </ul>
           </nav>
         </Container>
       </header>
     </div>
   )
+
 }
 
 export default Header
