d2d60ab052afbdbd131243ec86f7624dee3763e9
sushil2817 <tony281723@gmail.com>
2024-06-14 09:00:49 +0530

    create login page and signup page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -42,10 +42,32 @@ function Login() {
                 <h2 className='text-center text-2xl font-bold leading-tight'>Sign in to your account</h2>
                 <p className='mt-2 text-center text-base text-black/60'>
                     Don&apos;t have any account?&nbsp;
-                    <Link type='/signip' className='font-medium text-primary transition-all duration-200 hover:underline'>
+                    <Link type='/signUp' className='font-medium text-primary transition-all duration-200 hover:underline'>
                         Sign Up
                     </Link>
                 </p>
+                {error && <p className='text-red-600 mt-8 text-center'> {error}</p>}
+                <form onSubmit={handleSubmit(login)} className='mt-8'>
+                       <div className='space-y-5'>
+                               <Input label="Email:"
+                               placeholder="Enter your email"
+                               type="email"
+                               {...resgister("email",{
+                                required:true,
+                                validate:{
+                                  matchPatern:(value)=>/^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/.test(value) || "Email address must be a valid address"
+                                }
+                               })}
+                                />
+                                <Input label="Password:"
+                                type="password"
+                                placeholder="Enter your password" 
+                                 {...resgister("password",{required:true,
+                                 })}/>
+                                 <Button>Sign in</Button>
+                               
+                       </div>
+                </form>
       </div>
     </div>
   )
