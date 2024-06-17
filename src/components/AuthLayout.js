7a148cb9ccc7deb2f22cab41600d643b2ea8b37a
sushil2817 <tony281723@gmail.com>
2024-06-17 08:40:49 +0530

    trying to understand project

---
+++ src/components/AuthLayout.js
@@ -0,0 +1,11 @@
+ import React, {useEffect,useState} from 'react'
+ import { useSelector } from 'react-redux'
+ 
+ export default function Protected({children,authentication=true}) {
+   return (
+     <div>
+       
+     </div>
+   )
+ }
+ 
