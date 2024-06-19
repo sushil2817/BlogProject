98cda1f54c896edb3bce3f9b197a425343d8f4cd
sushil2817 <tony281723@gmail.com>
2024-06-19 08:43:40 +0530

    crating mega project

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,21 @@ import Header from "./Header/Header";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Post from "./Post";
+import PostForm from "./post-form/PostForm";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm
 }
