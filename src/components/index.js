6faa6baff2a152f0d04e7daff5a695f8e7e06c98
sushil2817 <tony281723@gmail.com>
2024-06-24 08:52:18 +0530

    one step closer to completing the project

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,8 @@ import Signup from "./Signup";
 import Login from "./Login";
 import Post from "./Post";
 import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 
 export {
     Header,
@@ -19,5 +21,7 @@ export {
     Signup,
     Login,
     Post,
-    PostForm
+    PostForm,
+    PostCard,
+    AuthLayout
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,36 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import { AuthLayout, Login } from './components/index.js'
+
+
+const router = createBrowserRouter([
+     {
+      path:'/',
+      element:<App/>,
+      children:[
+        {
+          path:'/',
+          element:<Home/>
+        },
+        {
+           path:"/login",
+           element:(
+            <AuthLayout authentication={false}>
+              <Login />
+            </AuthLayout>
+           )
+        }
+      ]
+     }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Provider store={store}>
-    <App />
-
+   <RouterProvider />
   </Provider>
   </React.StrictMode>,
 )
